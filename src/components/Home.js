b191867c4a07b9be619c4ff62e300c65b580582a
Abhay Dixit <adixit7386@gmail.com>
2023-02-18 11:32:34 +0530

    added project search option

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,31 @@ import React from "react";
 import Styled from "styled-components";
 import SearchProject from "./SearchProject";
 const Container = Styled.div``;
+const Header = Styled.div`
+margin:10px 20px;
+display:flex;
+align-items:center;
+justify-content:space-between;`;
 
+const Select = Styled.select`
+
+font-size:18px;
+border:none;
+padding:7px 12px;
+&:focus{
+  outline:none;
+}`;
+const Option = Styled.option`
+padding:5px 9px;`;
 const Home = () => {
   return (
     <Container>
+      <Header>
+        <Select>
+          <Option>Projects</Option>
+          <Option>People</Option>
+        </Select>
+      </Header>
       <SearchProject></SearchProject>
       <SearchProject></SearchProject>
       <SearchProject></SearchProject>
