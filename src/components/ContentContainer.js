0cd1ca25b6d1eee8937f840300be86c356e05f96
Abhay Dixit <adixit7386@gmail.com>
2023-03-08 12:38:00 +0530

    corrected socket.io endpoint

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -223,7 +223,7 @@ color:lightgrey;
 margin:0 auto;
 `;
 
-const ENDPOINT = "https://projectshare-eight.vercel.app";
+const ENDPOINT = "https://projectshare-eight.vercel.app/";
 var socket, selectedChatCompare;
 const ContentContainer = () => {
   const toggleBar = useSelector((state) => state.personbar.toggle);
